75c884c295230c83aa45e27a88a53c52a67bd034
bbjbc <jobc4550@gmail.com>
2024-10-23 18:52:56 +0900

    chore: 폴더 구조 변경

--- src/components/admin/home/user-list/UserList.js
+++ src/components/admin/home/user-list/UserList.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { sortByName } from '../../../utils/sortByName';
+import { sortByName } from '../../../../utils/sortByName';
 import './UserList.css';
 
 export const RenderUserList = async (container, filter = 'all') => {
