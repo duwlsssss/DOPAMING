217b5aff60a4b11d7d098a7b5bfc2511a3c74b57
bbjbc <jobc4550@gmail.com>
2024-10-24 11:13:53 +0900

    fix: 경로 수정

--- src/pages/admin/member/member-management/MemberManagement.js
+++ src/pages/admin/member/member-management/MemberManagement.js
@@ -1,4 +1,4 @@
-import { ApiClient } from '../../../../apis/apiClient';
+import { ApiClient } from '../../../../apis/ApiClient';
 import { Button } from '../../../../components';
 import './MemberManagement.css';
 
