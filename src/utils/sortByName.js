ac151b0fc68e95159b01707af9eb1633c9bd7b8d
bbjbc <jobc4550@gmail.com>
2024-10-25 15:02:40 +0900

    feat: 이름 정렬 함수에 휴가 승인 상태가 있을 경우에 우선 정렬

--- src/utils/sortByName.js
+++ src/utils/sortByName.js
@@ -1,5 +1,13 @@
 export const sortByName = users => {
   return users.sort((a, b) => {
+    // 휴가 승인 상태로 정렬 (대기 > 승인 > 거부)
+    if (a.abs_status && b.abs_status) {
+      if (a.abs_status === '대기' && b.abs_status !== '대기') return -1;
+      if (a.abs_status !== '대기' && b.abs_status === '대기') return 1;
+      if (a.abs_status === '승인' && b.abs_status === '거부') return -1;
+      if (a.abs_status === '거부' && b.abs_status === '승인') return 1;
+    }
+
     // 매니저가 먼저 나오도록 정렬
     if (a.user_position === '매니저' && b.user_position === '수강생') return -1;
     if (a.user_position === '수강생' && b.user_position === '매니저') return 1;
