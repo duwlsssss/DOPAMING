3d67dc7be3281186bf6f309acf657101c7ec9d08
bbjbc <jobc4550@gmail.com>
2024-10-27 00:38:26 +0900

    fix: import 구문 파일명 수정

--- src/pages/admin/member/member-management/MemberManagement.js
+++ src/pages/admin/member/member-management/MemberManagement.js
@@ -1,4 +1,4 @@
-import { ApiClient } from '../../../../apis/apiClient';
+import { ApiClient } from '../../../../apis/ApiClient';
 import { Button } from '../../../../components';
 import { Pagenation } from '../../../../components/index';
 import navigate from '../../../../utils/navigation';
