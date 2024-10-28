6ce22335cbda99ad71b04b0166b52f756884cfc4
bbjbc <jobc4550@gmail.com>
2024-10-28 11:29:04 +0900

    style: 반응형 스타일 추가

--- src/components/admin/home/user-list/UserList.js
+++ src/components/admin/home/user-list/UserList.js
@@ -5,7 +5,7 @@ import { Button } from '../../../ui/button/Button';
 import './UserList.css';
 
 export const RenderUserList = async (container, filter = 'all') => {
-  container.innerHTML = `<div class="loading">직원 정보를 가져오는 중입니다.</div>`;
+  container.innerHTML = `<div class="admin-loading">직원 정보를 가져오는 중입니다.</div>`;
 
   try {
     const response = await axios.get('../../../../server/data/users.json');
@@ -30,20 +30,20 @@ export const RenderUserList = async (container, filter = 'all') => {
     const sortedDisplayUsers = sortByName(displayUsers);
 
     container.innerHTML = `
-      <section class="member-section">
-        <div class="member-list">
-          <ul class="member-grid">
+      <section class="admin-member-section">
+        <div class="admin-member-list">
+          <ul class="admin-member-grid">
             ${sortedDisplayUsers
               .map(
                 (user, index) => `
-                <li class="member-item">
-                  <div class="member-info">
-                    <div class="status-dot active"></div>
-                    <img src="${user.user_image}" alt="${user.user_name}" class="member-avatar">
-                    <span class="role">${user.user_position}</span>
-                    <span class="name">${user.user_name}</span>
-                    <span class="email">${user.user_email}</span>
-                    <span class="phone">${user.user_phone}</span>
+                <li class="admin-member-item">
+                  <div class="admin-member-info">
+                    <div class="admin-status-dot active"></div>
+                    <img src="${user.user_image}" alt="${user.user_name}" class="admin-member-avatar">
+                    <span class="admin-role">${user.user_position}</span>
+                    <span class="admin-name">${user.user_name}</span>
+                    <span class="admin-email">${user.user_email}</span>
+                    <span class="admin-phone">${user.user_phone}</span>
                   </div>
                   <div class="admin-main-user-list-detail-button-container" data-index="${index}"></div>
                 </li>
@@ -87,7 +87,7 @@ export const RenderUserList = async (container, filter = 'all') => {
     }
 
     container.innerHTML = `
-      <div class="member-section error">
+      <div class="admin-member-section error">
         ${errorMessage}
       </div>
     `;
