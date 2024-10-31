3b43ae227f10c9382d7ad343dd06a79c55ac7cc0
bbjbc <jobc4550@gmail.com>
2024-10-31 16:21:43 +0900

    fix: 휴가 목록 롤백 및 버그 수정

--- src/components/admin/vacation-management/VacationList.js
+++ src/components/admin/vacation-management/VacationList.js
@@ -1,5 +1,5 @@
-import { getDatabase, ref, get } from 'firebase/database'; // Firebase Realtime Database 가져오기
-import { getAuth } from 'firebase/auth'; // Firebase Authentication 가져오기
+import axios from 'axios';
+
 import { Accordion } from '../../ui/accordion/Accordion';
 import { Button } from '../../ui/button/Button';
 import { sortByName } from '../../../utils/sortByName';
@@ -14,53 +14,25 @@ export const RenderAdminVacationManagementList = async (
   container.innerHTML = `<div class="loading">휴가 정보를 가져오는 중입니다.</div>`;
 
   try {
-    const db = getDatabase();
-    const absencesRef = ref(db, 'absences');
-    const usersRef = ref(db, 'Users');
-
-    const auth = getAuth();
-    const currentUser = auth.currentUser;
-
-    if (!currentUser) {
-      throw new Error('로그인된 사용자가 없습니다.');
-    }
-
-    console.log('로그인한 사용자 ID:', currentUser.uid);
-
-    const [absencesSnapshot, usersSnapshot] = await Promise.all([
-      get(absencesRef),
-      get(usersRef),
+    const [absencesResponse, usersResponse] = await Promise.all([
+      axios.get('../../../../server/data/absences.json'),
+      axios.get('../../../../server/data/users.json'),
     ]);
 
-    const absences = absencesSnapshot.exists() ? absencesSnapshot.val() : [];
-    const users = usersSnapshot.exists() ? usersSnapshot.val() : {};
-
-    console.log('부재 데이터:', absences);
-    console.log('사용자 데이터:', users);
+    const absences = absencesResponse.data;
+    const users = usersResponse.data;
 
     let absenceUsersData = absences.map(absence => {
-      const user = users[absence.user_id];
-
-      if (!user) {
-        console.warn(`사용자를 찾을 수 없습니다: ${absence.user_id}`);
-      }
-
-      console.log(
-        `부재 ID: ${absence.absenceId}, 사용자 ID: ${absence.user_id}, 사용자 이름: ${user ? user.user_name : '없음'}`,
-      );
-
+      const user = users.find(user => user.user_id === absence.user_id);
       return {
         ...absence,
-        user_name: user ? user.user_name : '알 수 없음',
-        user_image: user ? user.user_image : '',
-        user_phone: user ? user.user_phone : '',
-        user_position: user ? user.user_position : '',
+        user_name: user.user_name,
+        user_image: user.user_image,
+        user_phone: user.user_phone,
+        user_position: user.user_position,
       };
     });
-
-    console.log('휴가 사용자 데이터:', absenceUsersData);
     let sortedAbsenceUsersData = sortByName(absenceUsersData);
-    console.log('정렬된 휴가 사용자 데이터:', sortedAbsenceUsersData);
 
     // 필터링
     if (filter.type !== 'vacation-all') {
@@ -72,8 +44,6 @@ export const RenderAdminVacationManagementList = async (
       sortedAbsenceUsersData = sortedAbsenceUsersData.filter(
         absence => absence.abs_type === absType[filter.type],
       );
-
-      console.log('필터링된 휴가 유형 데이터:', sortedAbsenceUsersData);
     }
 
     if (filter.status !== 'approved-all') {
@@ -85,9 +55,73 @@ export const RenderAdminVacationManagementList = async (
       sortedAbsenceUsersData = sortedAbsenceUsersData.filter(
         absence => absence.abs_status === statusType[filter.status],
       );
-
-      console.log('필터링된 휴가 상태 데이터:', sortedAbsenceUsersData);
     }
+
+    // 다운로드 버튼
+    const downloadButton = new Button({
+      text: '다운로드',
+      color: 'skyblue',
+      shape: 'block',
+      padding: 'var(--space-xsmall) var(--space-small)',
+    });
+
+    // 승인, 거부, 대기 버튼
+    const renderButtons = status => {
+      switch (status) {
+        case '승인': {
+          const cancelApproveButton = new Button({
+            text: '승인 취소',
+            color: 'gray',
+            shape: 'block',
+            padding: 'var(--space-small) var(--space-large)',
+          });
+
+          return `
+            <div class="approval-button-group">
+              ${cancelApproveButton.outerHTML}
+            </div>
+          `;
+        }
+        case '거부': {
+          const cancelDenyButton = new Button({
+            text: '거부 취소',
+            color: 'gray',
+            shape: 'block',
+            padding: 'var(--space-small) var(--space-large)',
+          });
+
+          return `
+            <div class="approval-button-group">
+              ${cancelDenyButton.outerHTML}
+            </div>
+          `;
+        }
+        case '대기': {
+          const approveButton = new Button({
+            text: '승인하기',
+            color: 'skyblue-light',
+            shape: 'block',
+            padding: 'var(--space-small) var(--space-large)',
+          });
+
+          const rejectButton = new Button({
+            text: '거부하기',
+            color: 'coral',
+            shape: 'block',
+            padding: 'var(--space-small) var(--space-large)',
+          });
+
+          return `
+            <div class="approval-button-group">
+              ${approveButton.outerHTML}
+              ${rejectButton.outerHTML}
+            </div>
+          `;
+        }
+        default:
+          return '';
+      }
+    };
 
     // 아코디언 헤더
     const renderHeader = item => `
@@ -104,64 +138,40 @@ export const RenderAdminVacationManagementList = async (
     `;
 
     // 아코디언 컨텐츠
-    const renderContent = (item, index) => {
-      const downloadButton = new Button({
-        text: '다운로드',
-        color: 'skyblue',
-        shape: 'block',
-        padding: 'var(--space-xsmall) var(--space-small)',
-        onClick: () => {
-          const fileName = `FE_${item.user_name}_${item.abs_type}.pdf`;
-          const fileUrl = `path/to/your/files/${fileName}`; // 파일 URL (서버에 파일이 있어야 함)
-
-          // 로그 추가: 다운로드할 파일 정보 출력
-          console.log(`다운로드할 파일 이름: ${fileName}`);
-          console.log(`다운로드할 파일 URL: ${fileUrl}`);
-
-          // 파일 다운로드
-          const link = document.createElement('a');
-          link.href = fileUrl;
-          link.download = fileName;
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-        },
-      });
-
-      return `
-        <article class="detail-content">
-          <div class="detail-grid">
-            <section class="detail-item">
-              <h3 class="detail-label">휴가 제목</h3>
-              <div class="detail-value">${item.abs_title}</div>
-            </section>
-            
-            <div class="detail-item">
-              <h3 class="detail-label">휴가 기간</h3>
-              <div class="detail-value">
-                <time class="date">${item.abs_start_date}</time>
-                <span class="date-separator">~</span>
-                <time class="date">${item.abs_end_date}</time>
-              </div>
+    const renderContent = (item, index) => `
+      <article class="detail-content">
+        <div class="detail-grid">
+          <section class="detail-item">
+            <h3 class="detail-label">휴가 제목</h3>
+            <div class="detail-value">${item.abs_title}</div>
+          </section>
+          
+          <div class="detail-item">
+            <h3 class="detail-label">휴가 기간</h3>
+            <div class="detail-value">
+              <time class="date">${item.abs_start_date}</time>
+              <span class="date-separator">~</span>
+              <time class="date">${item.abs_end_date}</time>
             </div>
-
-            <section class="detail-item">
-              <h3 class="detail-label">첨부 파일</h3>
-              <div class="download-file">
-                <p class="detail-value">FE_${item.user_name}_${item.abs_type}.pdf</p>
-                ${downloadButton.outerHTML}
-              </div>
-            </section>
-
-            <section class="detail-item detail-content-box">
-              <h3 class="detail-label">휴가 내용</h3>
-              <p class="detail-value content-box" data-index="${index}">${item.abs_content}</p>
-            </section>
           </div>
 
-        </article>
-      `;
-    };
+          <section class="detail-item">
+            <h3 class="detail-label">첨부 파일</h3>
+            <div class="download-file">
+              <p class="detail-value">FE_${item.user_name}_${item.abs_type}.pdf</p>
+              ${downloadButton.outerHTML}
+            </div>
+          </section>
+
+          <section class="detail-item detail-content-box">
+            <h3 class="detail-label">휴가 내용</h3>
+            <p class="detail-value content-box" data-index="${index}">${item.abs_content}</p>
+          </section>
+        </div>
+
+        ${renderButtons(item.abs_status)}
+      </article>
+    `;
 
     const itemsPerPage = 6;
     const startIndex = (currentPage - 1) * itemsPerPage;
@@ -201,8 +211,15 @@ export const RenderAdminVacationManagementList = async (
     paginationContainer.appendChild(paginationElement);
     container.appendChild(paginationContainer);
   } catch (error) {
-    console.error('데이터를 불러오는 중 오류 발생:', error.message);
     let errorMessage = '데이터를 불러오는 중 오류가 발생했습니다.';
+
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        errorMessage = `Error ${error.response.status}: ${error.response.data.message || errorMessage}`;
+      } else if (error.request) {
+        errorMessage = '서버로부터 응답을 받지 못했습니다.';
+      }
+    }
 
     container.innerHTML = `
       <div class="admin-vacation-section error">
